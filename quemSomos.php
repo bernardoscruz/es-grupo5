5501aef04d170c846ef085234506c32cdc39218b
Bernardo Cruz <bsacruz@gmail.com>
2019-04-28 20:26:08 -0300

    alteração no quem somos

--- quemSomos.php
+++ quemSomos.php
@@ -12,25 +12,28 @@
 				<div class="thumbnail">
 					<h3 class="title">Missão</h3>
 					<p class="info">
-Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas aperiam veniam obcaecati adipisci blanditiis dolorem accusantium quo quasi tenetur voluptates assumenda voluptatem quos dolorum, repudiandae rerum ducimus nam ipsam eos.					</p>
+					Fornecer uma variedade de produtos de alta qualidade, auxiliando o cliente com toda e qualquer necessidade.			</p>
 				</div>
 			</div>
 			<div class="col-sm-4">
 				<div class="thumbnail">
 					<h3 class="title">Visão</h3>
 					<p class="info">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, cum architecto praesentium veritatis possimus, nemo quo totam dolorum laborum cupiditate tempora libero! Odit aliquam ut aperiam doloribus suscipit eum. Sunt.	</p>
+						Ser reconhecida por conseguir suprir todos os clientes das redondezas, se tornando uma loja de destaque na região.	</p>
 				</div>
 			</div>
 			<div class="col-sm-4">
 				<div class="thumbnail">
 					<h3 class="title">Valores</h3>
 					<p class="info">
-						<strong>Respeito</strong>: Acreditamos que esta é a base de toda e qualquer relação, seja ela pessoal ou profissional.<br>
-						<strong>Profissionalismo</strong>: Comprometimento e seriedade, com foco na qualidade dos serviços prestados.<br>
-						<strong>Parceria</strong>: A união de esforços com nossos parceiros e colaboradores resultará sempre em grandes conquistas para todas as partes.<br>
-						<strong>Inovação</strong>: Buscar soluções inovadoras e eficazes para a agilidade e qualidade de nossos resultados.<br>
-						<strong>Transparência</strong>: Agir com seriedade, honestidade e clareza em nossos relacionamentos.<br>
+						<strong>Respeito</strong>: Acreditamos que esta é a base de toda e qualquer relação, seja ela pessoal ou profissional.
+						<br>
+						<br>
+						<strong>Profissionalismo</strong>: Comprometimento e seriedade, com foco na qualidade dos produtos da loja.
+						<br>
+						<br>
+						<strong>Parceria</strong>: A união de esforços com nossos parceiros e clientes resultará sempre em grandes conquistas para todas as partes.
+						<br>
 					</p>
 				</div>
 			</div>
