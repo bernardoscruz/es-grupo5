45e19e52adecc1bbf729c9955adb9bd90242ba78
Lucas Piazzi <lpiazzi26@gmail.com>
2019-06-11 22:02:29 -0300

    Correção do redirecionamento após o registro

--- controllers/login/verifica-registro.php
+++ controllers/login/verifica-registro.php
@@ -7,6 +7,10 @@ $usuario = buscaLogin($connect, $email);
 if($_POST['email'] != $usuario['email'] && cadastraUsuario($connect, $_POST['nome'], $_POST['email'], $_POST['senha'], $_POST['categoria'], $_POST['cidade'], $_POST['estado'])) {
     $usuarioId = mysqli_insert_id($connect);
     mysqli_query($connect, "INSERT INTO clientes (cnpj, usuario_id) values ('{$_POST['cnpj']}', '{$usuarioId}')");
+    session_start();
+    $_SESSION['islogged'] = true;
+    $_SESSION['email'] = $email;
+    $_SESSION['categoria'] = $usuario['categoria'];
     header("Location: ../../views/home.php?cadastrado=1");
 }
 else {
